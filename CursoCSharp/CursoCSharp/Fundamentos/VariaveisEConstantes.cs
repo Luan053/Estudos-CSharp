2089516c0952768d82ce4aa60febda48b8d3ff10
Luan053 <luanrschulzz@gmail.com>
2023-05-24 00:18:09 -0300

    Variaveis e constantes

--- CursoCSharp/CursoCSharp/Fundamentos/VariaveisEConstantes.cs
+++ CursoCSharp/CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -6,58 +6,23 @@ namespace CursoCSharp.Fundamentos
 {
     class VariaveisEConstantes
     {
-        public static void Executar() {
-            // area da circunferencia
-            double raio = 4.5;
+        public static void Executar()
+        {
+            double raio = 2.5;
             const double PI = 3.14;
 
-            raio = 5.5;
-            // PI = 3.1415;
-
             double area = PI * raio * raio;
+
             Console.WriteLine(area);
             Console.WriteLine("Área é " + area);
 
-            // Tipos internos
+            bool testeBool = true;
+            Console.WriteLine(testeBool);
 
-            bool estaChovendo = true;
-            Console.WriteLine("Está chovendo " + estaChovendo);
+            int idade = 19;
+            Console.WriteLine("Tenho" + idade + "anos");
 
-            byte idade = 45;
-            Console.WriteLine("Idade " + idade);
-
-            sbyte saldoDeGols = sbyte.MinValue;
-            Console.WriteLine("Saldo de Gols " + saldoDeGols);
-
-            short salario = short.MaxValue;
-            Console.WriteLine("Salário " + salario);
-
-            int menorValorInt = int.MinValue; // Mais usado dos inteiros!
-            Console.WriteLine("Menor int " + menorValorInt);
-
-            uint populacaoBrasileira = 207_600_000;
-            Console.WriteLine("População Brasileira " + populacaoBrasileira);
-
-            long menorValorLong = long.MinValue;
-            Console.WriteLine("Menor long " + menorValorLong);
-
-            ulong populacaoMundial = 7_600_000_000;
-            Console.WriteLine("População Mundial " + populacaoMundial);
-
-            float precoComputador = 1299.99F;
-            Console.WriteLine("Preço Computador " + precoComputador);
-
-            double valorDeMercadoDaApple = 1_000_000_000_000.00; // Mais usado dos reais!
-            Console.WriteLine("Valor Apple " + valorDeMercadoDaApple);
-
-            decimal distanciaEntreEstrelas = decimal.MaxValue;
-            Console.WriteLine("Distância entre Estrelas " + distanciaEntreEstrelas);
-
-            char letra = 'b';
-            Console.WriteLine("Letra " + letra);
-
-            string texto = "Seja bem vindo ao Curso de C#!";
-            Console.WriteLine(texto);
+            
         }
     }
 }
